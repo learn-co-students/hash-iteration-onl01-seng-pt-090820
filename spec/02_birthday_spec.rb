0ae3e9473d1af61b062a18f57a31dd27e22299a9
Anthony Leon <acidknights89@gmail.com>
2020-09-16 03:29:45 +0000

    Done.

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -16,12 +16,22 @@ end
 ## BONUS ##
 ## Change "xit" to "it" in the test block to enable
 describe "#age_appropriate_birthday" do
-  xit "only prints the birthday greeting if the birthday kid 12 or younger" do
+  it "only prints the birthday greeting if the birthday kid 12 or younger" do
     birthday_kids = {
       "Timmy" => 9,
       "Sarah" => 6,
       "Amanda" => 27
     }
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    elsif age > 12
+    puts "You are too old for this."
+  end
+end
+
+end
 
     expect{age_appropriate_birthday(birthday_kids)}.to output("Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdout
 
