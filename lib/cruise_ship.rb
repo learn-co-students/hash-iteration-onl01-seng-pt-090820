31fbcbc586d5ad498c39521c17cb856921c1a859
DavidRMorphew <morphewd@umich.edu>
2020-09-16 14:54:05 -0400

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,11 +1,31 @@
-# passengers = {
+#  passengers = {
 # suite_a: "Amanda Presley", 
 # suite_b: "Seymour Hoffman", 
 # suite_c: "Alfred Tennyson", 
 # suite_d: "Charlie Chaplin", 
 # suite_e: "Crumpet the Elf"
-# }
+# }#
+
+# def select_winner(passengers)
+#   winner = ""
+#   passengers.each do |suite, name|
+#     if suite == :suite_a && name.start_with?("A")
+#       winner = name
+#     end
+#   end
+#   winner
+# end
+
+# def select_winner(passengers)
+# passengers.each do |suite, name|
+#     if suite == :suite_a && name.start_with?("A")
+#       return name
+#     end
+#   end
+# end
 
 def select_winner(passengers)
-  # add the code snippet here!
+  passengers.each do |suite, name|
+    return name if suite == :suite_a && name.start_with?("A")
+  end
 end
