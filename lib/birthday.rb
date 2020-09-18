8eeaeafcd3ad2a45e59dd5aa5a997168dd651498
S1umish <sunitee.mishra@gmail.com>
2020-09-18 14:07:16 -0400

    Done.

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,17 +6,61 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  # if age < 12
-  #   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  # else
-  #     returns " You are older than 12"
-  #   end
   end  # add your code snippet here!
 end
-  ##hsh.index(value) => key
+
+#xit "only prints the birthday greeting if the birthday kid 12 or younger" do
+def age_appropriate_birthday(birthday_kids) 
+    age_appropriate_birthday =""
+    birthday_kids.each do |kids_name, age|
+      if age < 12
+        puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+      else
+        puts "You are too old for this."
+      end
+
+      # add the code snippet here!
+      
+    end
+end
+
+#puts "Happy Birthday Timmy! You are now 9 years old!\nHappy Birthday Sarah! You are now 6 years old!\nYou are too old for this.\n").to_stdo
+#   # name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+#   # name_hash = {:blake => 10, :ashley => 50, :adam => 17}
+#   # name_hash = {}
+
+# def key_for_min_value(name_hash)
+#   lowest_key = name_hash.reduce do |key, value|
+#     key.last > value.last ? value : key
+#   end
+#     if name_hash == {}
+#       lowest_key
+#     else
+#       lowest_key.first
+#     end
+# end
+
+
+# name_hash = {:blake => 500, :ashley => 2, :adam => 1}
+# name_hash = {:blake => 10, :ashley => 50, :adam => 17}
+# name_hash = {}
+
+# def key_for_min_value(name_hash)
+#   lowest_key = name_hash.reduce do |key, value|
+#     key.last > value.last ? value : key
+#   end
+#     if name_hash == {}
+#       lowest_key
+#     else
+#       lowest_key.first
+#     end
+# end
+  
+
+
+##hsh.index(value) => key
   #birthday_kids(9)
 
 # def key_for_min_value(hash)
@@ -37,7 +81,6 @@ end
 #   hash.keys[values.index(values.min).to_i]
 # end
 # key_for_min_value(birthday_kids)
-
 
 
 
